ede48758bd7a97ea35466ae352c2804bccf455d6
zaivi <phanvigiaii@email.com>
2023-09-18 17:08:14 +0700

    added config and base constants

---
+++ src/constants/base.py
@@ -0,0 +1,6 @@
+BeginningTaskMessage        = "beginning task = %s info input = %v"
+TaskErrorMessage            = "task = %s is failed with err = %v"
+GetEntityErrorMessage       = "get entity = %s info input = %v fail with err = %v"
+UpdateEntityErrorMessage    = "update entity = %s info input = %v fail with err = %v"
+CreateEntityErrorMessage    = "create entity = %s info input = %v fail with err = %v"
+GetEntityClientErrorMessage = "get entity client = %s info input = %v fail with err = %v"
---
+++ src/utils/leetcode/config.py
@@ -0,0 +1,15 @@
+ssi_headers = {
+    "authority": "leetcode.com",
+    "accept": "*/*",
+    "accept-language": "en-US,en;q=0.9,vi;q=0.8,es;q=0.7",
+    "content-type": "application/json",
+    "origin": "https://leetcode.com",
+    "referer": "https://leetcode.com/problemset/all/",
+    "sec-ch-ua": "\"Google Chrome\";v=\"111\", \"Not(A:Brand\";v=\"8\", \"Chromium\";v=\"111\"",
+    "sec-ch-ua-mobile": "?0",
+    "sec-ch-ua-platform": "\"macOS\"",
+    "sec-fetch-dest": "empty",
+    "sec-fetch-mode": "cors",
+    "sec-fetch-site": "same-origin",
+    "user-agent": ""
+}
